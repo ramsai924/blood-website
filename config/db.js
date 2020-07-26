53f8a9615749f5aea8b4ca275cf465f90b610fbb
ramsai924 <ramsai924@gmail.com>
2020-07-26 07:49:35 +0530

    app

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose')
 
-const mongoyrl = process.env.MOGO_DEV_URL;
+
 const db = mongoose
-  .connect(`mongodb://localhost:27017/bloodDonar`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  //mongodb+srv://ramsai:14211@0591@cluster0.hjpoy.mongodb.net/blood-web?retryWrites=true&w=majority
+  .connect(
+    `mongodb+srv://ramsai:14211@0591@cluster0.hjpoy.mongodb.net/blood-web?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log("connected to db ");
   })
